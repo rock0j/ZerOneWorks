dee7cc3935f44abb51ff327d9b834e1f0a91b3e6
zolawj <tigerlzola@gmail.com>
2014-07-29 15:13:46 +0800

    对文档结构增加说明
    对文档结构增加说明

--- models/KeyCmd.js
+++ models/KeyCmd.js
@@ -3,23 +3,23 @@ var mongodb = require('./mongodb.js');
 var Schema = mongodb.mongoose.Schema;
 
 var KeyCmdSchema = new Schema({
-    nSMSID			:Number,
-    nPlatfromID		:Number,
-    sServiceID		:String,
-    sServiceNO		:String,
-    nAppID			:Number,
-    sAppNO			:String,
-    sKey			:String,
-    sParm			:String,
-    nKeyType		:Number,
-    sContendID		:String,
-    fFunctionName	:String,
-    lCreateTime		:Number,
-    nCreateUserID	:Number,
-    lUpdateTime		:Number,
-    nUpdateUserID	:Number,
-    nStatus			:Number,
-    sNotes			:String
+    nSMSID			:Number,     //对接的消息平台ID
+    nPlatfromID		:Number,     //平台ID 0-999 保留给自己用
+    sServiceID		:String,     //微信公众识别号或者其他的识别号
+    sServiceNO		:String,     //微信或其他平台的识别token字段
+    nAppID			:Number,     //接入应用标识 0-999 保留给自己用
+    sAppNO			:String,     //接入应用的token字段
+    sKey			:String,     //消息指令关键字
+    sParm			:String,     //消息指令参数
+    nKeyType		:Number,     //消息指令类型 1首次访问 2菜单 3文字命令  4带参数文字命令 5动态文字命令 6带上下文的命令 7搜索
+    sContentID		:String,     //对应的内容ID 无表示为动态命令
+    fFunctionName	:String,     //执行动态指令时对应的方法名称
+    lCreateTime		:Number,     //创建时间
+    nCreateUserID	:Number,     //创建人
+    lUpdateTime		:Number,     //修改时间
+    nUpdateUserID	:Number,     //修改人
+    nStatus			:Number,     //指令状态
+    sNotes			:String      //备注
 });
 
 var KeyCmd = mongodb.mongoose.model('KeyCmds', KeyCmdSchema);
